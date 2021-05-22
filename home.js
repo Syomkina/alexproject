3cf7b6d09d5fc1c7ffb388fc7d6d191f8486518d
student3 <student3@student3.com>
2021-05-22 15:58:23 +0300

    updates

--- home.js
+++ home.js
@@ -14,6 +14,7 @@
 // function addItem(self) {
 // 	const li = document.createElement('li')
 //   const ul = document.querySelector('.spisok')
+  
 //   let text = self.querySelector('p').innerText
 //   console.log(text)
 //   li.innerText = text
@@ -26,66 +27,40 @@
 // }
 
 
-let counts = [0,0,0,0,0]
 function addItem(self) { 
   const li = document.createElement('li') 
-  const button = document.createElement('button')
-  button.classList.add('addItem2')
+  const remove1 = document.createElement('button')
   li.classList.add('addItem') 
+  remove1.classList.add('addItem2')
   const ul = document.querySelector('.spisok') 
+  remove1.classList.add('addItem2') 
   let text = self.querySelector('p').innerText 
   console.log(text) 
-  li.innerText = text
-  li.append(button)
+  li.innerText = text 
+  ul.append(remove1)
   ul.append(li)  
-
-  // const allItems = document.querySelectorAll('.addItem').forEach(element => {
-  //   if (element.innerText == text){
-  //     counts[index]++
-  //     element.innerText = text + counts[index]
-
-  //   }
-  // })}
+  console.log(li) 
   
+} 
 
-  const btnRemove2 = document.querySelectorAll('.addItem2')
-  for (let i = 0; i < btnRemove2.length; i++) {
-    btnRemove2[i].onclick = function(){
-      console.log('hello')
-      this.parentElement.remove()
-    }
-  }
-   
- }
-
-
+const btnRemove2 = document.querySelectorAll('.addItem2')
 const btnRemove = document.querySelector('.remove') 
 btnRemove.addEventListener('click', removeItems) 
  
+function removeItems2() {
+  const allAdded2 = document.querySelectorAll('.addItem2')
+  for (const item of allAdded2) {
+    item.remove()
+  }
+}
+
 function removeItems() { 
-  const allAdded = document.querySelectorAll('.addItem')
+  const allAdded = document.querySelectorAll('.addItem') 
   console.log(allAdded) 
   for (const item of allAdded) { 
     item.remove()    
   } 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
